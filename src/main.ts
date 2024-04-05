5b7c2fa764d6db463cc95447d23dfc2b974fdd27
VincentFourcroy <vincentfourcroy@hotmail.fr>
2024-04-05 14:14:09 +0200

    added swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS testing')
+    .setDescription('Basic API made in order to learn how to use NestJS')
+    .setVersion('1.0')
+    .addTag('learning')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
